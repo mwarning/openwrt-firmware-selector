0ef910460d6b091ac6fb5de73f7c44a066cf60f0
Moritz Warning <moritzwarning@web.de>
2020-02-21 15:03:06 +0100

    udpate translations

--- i18n.js
+++ i18n.js
@@ -16,7 +16,6 @@ var translations = {
 		'tr-sysupgrade-help': 'Sysupgrade Abbilder werden für Geräte verwendet, die bereits OpenWrt laufen haben. Es ist möglich, existierende Einstellungen beizubehalten.',
 		'tr-kernel-help': 'Linux Kernel als separates Abbild.',
 		'tr-rootfs-help': 'Das Root Dateisystem als separates Abbild.',
-		'tr-tftp-help': 'Image das per tftp Protokol mit dem boot loader geladen wird.',
 		'tr-sdcard-help': 'Image für SD Speicherkarten.',
 		'tr-tftp-help': 'TFTP Dateien können verwendet werden, um ein Gerät über die TFTP Method des Bootloader zu flashen.',
 		'tr-other-help': 'Sonstiger Imagetyp.'
@@ -30,16 +29,15 @@ var translations = {
 		'tr-customize': 'Customize',
 		'tr-request-build': 'Request Build',
 		'tr-model': 'Modell:',
-		'tr-target': 'Platform',
+		'tr-target': 'Platform:',
 		'tr-version': 'Version:',
 		'tr-release-downloads': 'Downloads',
 		'tr-custom-downloads': 'Custom Downloads',
-		'tr-factory-help': 'Factory images are for flashing routers with OpenWrt for the first time using the web interface of the original firmware.',
-		'tr-sysupgrade-help': 'Sysupgrade images are for flashing routers that already run OpenWrt. The image can be applied using the web interface or the console.',
+		'tr-factory-help': 'Factory images are for flashing routers with OpenWrt for the first time. Usually via the web interface of the original firmware.',
+		'tr-sysupgrade-help': 'Sysupgrade images are for flashing routers that already run OpenWrt. The image can be applied using the web interface or the terminal.',
 		'tr-kernel-help': 'Linux kernel as a separate image.',
 		'tr-rootfs-help': 'Root file system as a separate image.',
-		'tr-tftp-help': 'Image that can be applied using the tftp meachnism of the boot loader',
-		'tr-sdcard-help': 'Image that is meant to be flashed on an SD-Card.',
+		'tr-sdcard-help': 'Image that is meant to be flashed onto a SD-Card.',
 		'tr-tftp-help': 'TFTP images are used to flash a device via the TFTP method of the bootloader.',
 		'tr-other-help': 'Other image type.'
 	}
