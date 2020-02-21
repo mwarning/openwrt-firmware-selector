c711a2c3c3f98fd4dac626898752864eda514c11
Moritz Warning <moritzwarning@web.de>
2020-02-21 14:45:42 +0100

    disable sasu backend by default

--- config.js
+++ config.js
@@ -6,11 +6,12 @@ var config = {
   showHelp: true,
   // Files to get data from
   versions: {
-  //'SNAPSHOT': '/api/names/SNAPSHOT' // when using asu backend
+  //'SNAPSHOT': '/api/names/SNAPSHOT', // when using sasu backend
     'SNAPSHOT': 'names-SNAPSHOT.json',
     '19.07.1': 'names-19.07.1.json',
     '18.06.7': 'names-18.06.7.json'
   },
-  // Build custom images (Attented Sysupgrade server)
-  asu_url: '/api/build'
+  // Build custom images
+  // See https://github.com/aparcar/sasu
+  //asu_url: '/api/build'
 };
