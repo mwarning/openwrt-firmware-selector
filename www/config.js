d7b5915ac64650418ea1ccf87f5332ccf978e119
Moritz Warning <moritzwarning@web.de>
2020-11-24 20:18:52 +0100

    config.js: add optional image_url and info_url
    Allow image_url and info_url to be set in the config.js file.
    
    Signed-off-by: Moritz Warning <moritzwarning@web.de>

--- www/config.js
+++ www/config.js
@@ -3,14 +3,23 @@
 var config = {
   // Show help text for images
   showHelp: true,
+
   // Image overview file or path to the ASU API
   versions: {
     "18.06.8": "../misc/18.06.8/",
     "19.07.4": "../misc/19.07.4/",
     SNAPSHOT: "../misc/SNAPSHOT/",
   },
+
   // Pre-selected version
   default_version: "19.07.4",
+
+  // Image download URL
+  //image_url: "https://downloads.openwrt.org/releases/{version}/{target}",
+
+  // Info link URL
+  //info_url: "https://openwrt.org/start?do=search&id=toh&q={title}",
+
   // Build custom images
   // See https://github.com/aparcar/asu
   //asu_url: 'https://chef.libremesh.org'
