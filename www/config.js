beac68457f4376ec7e1df8fd34dbc251d05869ef
Moritz Warning <moritzwarning@web.de>
2020-11-25 10:19:10 +0100

    config.js: improve comments
    Signed-off-by: Moritz Warning <moritzwarning@web.de>

--- www/config.js
+++ www/config.js
@@ -4,23 +4,23 @@ var config = {
   // Show help text for images
   showHelp: true,
 
-  // Image overview file or path to the ASU API
+  // Path to overview.json file or URL to the ASU API
   versions: {
     "18.06.8": "../misc/18.06.8/",
     "19.07.4": "../misc/19.07.4/",
     SNAPSHOT: "../misc/SNAPSHOT/",
   },
 
-  // Pre-selected version
+  // Pre-selected version (optional)
   default_version: "19.07.4",
 
-  // Image download URL
+  // Image download URL (optional)
   //image_url: "https://downloads.openwrt.org/releases/{version}/{target}",
 
-  // Info link URL
+  // Info link URL (optional)
   //info_url: "https://openwrt.org/start?do=search&id=toh&q={title}",
 
-  // Build custom images
+  // Build custom images (optional)
   // See https://github.com/aparcar/asu
   //asu_url: 'https://chef.libremesh.org'
 };
