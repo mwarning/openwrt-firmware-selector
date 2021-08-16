6abd5536f00600be8af68b3b9377703d63d93594
Moritz Warning <moritzwarning@web.de>
2021-08-16 23:36:25 +0200

    asu: remove attended sysupgrade
    Remove as this feature is not supported anymore.
    
    Signed-off-by: Moritz Warning <moritzwarning@web.de>

--- www/config.js
+++ www/config.js
@@ -4,7 +4,7 @@ var config = {
   // Show help text for images
   show_help: true,
 
-  // Path to overview.json file or URL to the ASU API
+  // Path to were overview.json can be found
   versions: {
     "18.06.9": "../misc/18.06.9/",
     "19.07.5": "../misc/19.07.5",
@@ -19,8 +19,4 @@ var config = {
 
   // Info link URL (optional)
   info_url: "https://openwrt.org/start?do=search&id=toh&q={title} @toh",
-
-  // Build custom images (optional)
-  // See https://github.com/aparcar/asu
-  //asu_url: 'https://chef.libremesh.org'
 };
